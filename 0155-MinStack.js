4c586b4b6a03f3fac49b6e7c1257e7cf054db666
Albert Ahn <58611720+albertfromsd@users.noreply.github.com>
2020-05-10 19:30:40 -0700

    Finished 0155-minStack with non-optimal memory complexity

--- 0155-MinStack.js
+++ 0155-MinStack.js
@@ -1,2 +1,80 @@
 // 155
 // https://leetcode.com/problems/min-stack
+
+class Stack {
+    constructor(){
+        this.items = [];
+        this.minTracker = [];
+    };
+
+    push(item) {
+        if( this.items.length == 0 ) {
+            this.minTracker.push(item);
+        } else if( item < this.minTracker[this.minTracker.length-1] ) {
+            this.minTracker.push(item);
+        } else {
+            this.minTracker.push(this.minTracker[this.minTracker.length-1])
+        };
+        this.items.push(item);
+    };
+
+    pop() {
+        if ( this.isEmpty() ) {
+            return null;
+        } else {
+            console.log("pop entered")
+            this.minTracker.pop();
+            return this.items.pop();
+        };
+    };
+
+    size() {
+        return this.items.length;
+    };
+
+    peek() {
+        return this.items[this.items.length - 1];
+    };
+
+    isEmpty() {
+        return this.items.length === 0;
+    };
+
+
+    printAll() {
+        let i = this.items.length-1;
+        while( i >= 0) {
+            console.log(this.items[i]);
+
+            i -= 1;
+        };
+    };
+
+    printMinStack() {
+        let i = this.minTracker.length-1;
+        while( i >= 0) {
+            console.log(this.minTracker[i]);
+
+            i -= 1;
+        };
+    };
+
+    // [3.2] Stack Min
+    min() {
+        return this.minTracker[this.minTracker.length-1]
+    }
+};
+
+const newStack = new Stack()
+newStack.push(-1)
+newStack.push(5)
+newStack.push(1)
+newStack.push(7)
+newStack.push(1)
+newStack.push(8)
+newStack.push(6)
+newStack.push(-3)
+newStack.printAll()
+console.log("-------------")
+console.log( "Min: ", newStack.min() )
+// newStack.pop()
