5e3ef68ef52066c4e387d62dc7766980e19fe82f
Albert Ahn <58611720+albertfromsd@users.noreply.github.com>
2020-05-12 16:48:34 -0700

    1108-DefangingIPAddress Completed

--- 1108-DefangingIPAddress.js
+++ 1108-DefangingIPAddress.js
@@ -15,3 +15,23 @@
 
 // Constraints:
 // The given address is a valid IPv4 address.
+
+// Runtime: 52 ms, faster than 70.32% of JavaScript online submissions for Defanging an IP Address.
+// Memory Usage: 33.9 MB, less than 100.00% of JavaScript online submissions for Defanging an IP Address.
+
+function defangIPAddress(str) {
+    let newArr = str.split('');
+
+    for( let i=0; i<newArr.length; i++ ) {
+        if( newArr[i] === '.') {
+            newArr[i] = '[.]';
+        };
+    };
+
+    return newArr.join('');
+};
+
+const str1 = "1.1.1.1";
+const str2 = "255.100.50.0";
+
+console.log( defangIPAddress(str2) );
