c779b4b80b54e1c8f15b57b24b666789f808d671
Albert Ahn <58611720+albertfromsd@users.noreply.github.com>
2020-05-30 19:46:48 -0700

    Garbage removal

--- 2-Medium/u0846-HandOfStraights.js
+++ 2-Medium/u0846-HandOfStraights.js
@@ -26,21 +26,18 @@
 
 // Runtime: 604 ms, faster than 25.33% of JavaScript online submissions for Hand of Straights.
 // Memory Usage: 42.4 MB, less than 100.00% of JavaScript online submissions for Hand of Straights.
-function handOfStraights( hand, w ) {
-    if( w === 1 ) return true;
-    if( hand.length % w !== 0 ) return false;
+function handOfStraights( hand, len ) {
+    if( len === 1 ) return true;
+    if( hand.length % len !== 0 ) return false;
 
     let setOfStraights = [];
     let handCopy = [...hand];
     function arrangeStraights() {
         let subSet = [];
-        while( subSet.length < w && handCopy.length ) {
-            // need an if-statement to check for validity of straight
-            // check for validity of tempMin is equal to +1 of last element in subSet array
+        while( subSet.length < len && handCopy.length ) {
             if( subSet.length === 0 ) {
                 let tempMin = Math.min( ...handCopy );
                 let tempMinIndex = handCopy.indexOf( tempMin );
-                // if( tempMin !== ( subSet[subSet.length-1] + 1 || !subSet[0] ) ) break;
                 subSet.push( ...handCopy.splice( tempMinIndex, 1 ) );
             } else {
                 let tempMinIndex = handCopy.indexOf( subSet[subSet.length-1] + 1 );
@@ -59,7 +56,7 @@ function handOfStraights( hand, w ) {
     };
     
     for( let i=0; i<setOfStraights.length; i++ ) {
-        for( let j=1; j<w; j++ ) {
+        for( let j=1; j<len; j++ ) {
             if( setOfStraights[i][j] !== setOfStraights[i][j-1]+1 ) return false;
         }
     };
@@ -72,4 +69,4 @@ const hand2 = [1,2,3,4,5];
 const hand3 = [1,2,3,4,5,6];
 const hand4 = [5, 1];
 
-console.log( handOfStraights( hand1, 3 ) );
+console.log( handOfStraights( hand4, 2 ) );
