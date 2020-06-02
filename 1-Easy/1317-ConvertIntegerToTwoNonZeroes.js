95a52e6ace502edf5f9439844be7421a8650a747
Albert Ahn <58611720+albertfromsd@users.noreply.github.com>
2020-06-01 22:28:34 -0700

    1317-LuckyInteger: Completed with runtime faster than 23%; memory less than 100%

---
+++ 1-Easy/1317-ConvertIntegerToTwoNonZeroes.js
@@ -0,0 +1,52 @@
+// 1317 Easy
+// https://leetcode.com/problems/convert-integer-to-the-sum-of-two-no-zero-integers/
+
+// Given an integer n. No-Zero integer is a positive integer which doesn't contain any 0 in its decimal representation.
+
+// Return a list of two integers [A, B] where:
+
+// A and B are No-Zero integers.
+// A + B = n
+// It's guarateed that there is at least one valid solution. 
+// If there are many valid solutions you can return any of them.
+
+ 
+
+// Example 1:
+// Input: n = 2
+// Output: [1,1]
+// Explanation: A = 1, B = 1. A + B = n and both A and B don't contain any 0 in their decimal representation.
+
+// Example 2:
+// Input: n = 11
+// Output: [2,9]
+
+// Example 3:
+// Input: n = 10000
+// Output: [1,9999]
+
+// Example 4:
+// Input: n = 69
+// Output: [1,68]
+
+// Example 5:
+// Input: n = 1010
+// Output: [11,999]
+ 
+
+// Constraints:
+// 2 <= n <= 10^4
+
+
+// NO ZEROES anywhere
+function twoNonZeroes( num ) {
+    
+};
+
+const n1 = 2;
+const n2 = 11;
+const n3 = 1000;
+const n4 = 69;
+const n5 = 1010;
+
+twoNonZeroes( n1 );
