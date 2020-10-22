0490a11275af1e11175c1272222dc143ace07623
Albert Ahn <58611720+albertfromsd@users.noreply.github.com>
2020-10-21 20:02:47 -0700

    0938-RangeSumOfBST && 0701-InsertIntoBST: Completed with middling rt and mu

---
+++ 1-Easy/c0938-RangeSumOfBST.js
@@ -0,0 +1,36 @@
+// 938 Easy
+// https://leetcode.com/problems/range-sum-of-bst/
+
+// Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+// The binary search tree is guaranteed to have unique values.
+
+
+// Example 1:
+// Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+// Output: 32
+
+// Example 2:
+// Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+// Output: 23
+ 
+// Note:
+// The number of nodes in the tree is at most 10000.
+// The final answer is guaranteed to be less than 2^31.
+
+
+// Runtime: 220 ms, faster than 80.46% of JavaScript online submissions for Range Sum of BST.
+// Memory Usage: 62.2 MB, less than 10.14% of JavaScript online submissions for Range Sum of BST.
+const rangeSumBST = function(root, L, R) {
+    if( !root ) return;
+    
+    let sum = 0, stack = [root];
+    while( stack.length ) {
+        let cur = stack.pop();
+        if( cur.val >= L && cur.val <= R ) sum += cur.val;
+        
+        if( cur.val >=L && cur.left ) stack.push(cur.left);
+        if( cur.val <= R && cur.right ) stack.push(cur.right);
+    }
+    
+    return sum;
+};
